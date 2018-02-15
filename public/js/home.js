bdafecc147891c38e7f8c276d6f042f1e590d6c5
Ankur Rai <rai.ankur0007@gmail.com>
2018-02-15 17:00:45 +0530

    [2] [Ankur/Ravinder] - sending request for game id
    What changes have you made?
    
    * added function to send request for game id
    
    Why do we need this change?
    
    * to get game id
    
    What can this change impact?
    
    * by sharing the game id, our friends can join the game´

--- public/js/home.js
+++ public/js/home.js
@@ -1,14 +1,30 @@
-const getInputBox=function (label,name) {
-  return `${label}: <input type="text" name=${name} ><br>`;
+let createRequest = function(callback, url, reqBody = null, method = "GET") {
+  let xhr = new XMLHttpRequest();
+  xhr.onload = callback;
+  xhr.open(method, url);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.send(reqBody);
 };
 
-const getButton=function (type, name, text, cb = null) {
+const getInputBox = function(label, name) {
+  return `${label}: <input type="text" name=${name} ><br>`;
+};
+const showGameId=function () {
+  return;
+};
+
+const reqGameId=function () {
+  let name=document.getElementsByName("name")[0].value;
+  createRequest(showGameId,"/gameId", `name=${name}`,"POST");
+};
+
+const getButton = function(type, name, text, cb = null) {
   return `<button type=${type} name=${name} onclick=${cb}()>${text}</button>`;
 };
 
-const showCreateGameForm=function () {
-  let container=document.getElementById('container');
-  let inputBox = getInputBox("Name","name");
-  let gameCreationButton = getButton("button", "name", "CREATE GAME");
+const showCreateGameForm = function() {
+  let container = document.getElementById('container');
+  let inputBox = getInputBox("Name", "name");
+  let gameCreationButton = getButton("button","name","CREATE GAME",reqGameId);
   container.innerHTML = `${inputBox}<br>${gameCreationButton}`;
 };
