7fce2f7337cb110f84061bc90baadb4133d1c6f8
Ankur Rai <rai.ankur0007@gmail.com>
2018-02-16 11:18:41 +0530

    [2] [Ankur/Ravinder] - creating a new game
    What changes have you made?
    
    * changed file path
    
    Why do we need this change?
    
    * to fix the bug
    
    What can this change impact?
    
    * it will fix the bug

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const cookieParser =require('cookie-parser');
 const Game=require('./src/models/game.js');
 const app = express();
 const log = require("./src/handlers/logger.js").log;
-const CompositeHandler = require('./src/handlers/CompositeHandler.js');
-const AddPlayerHandler = require('./src/handlers/AddPlayerHandler.js');
+const CompositeHandler = require('./src/handlers/compositeHandler.js');
+const AddPlayerHandler = require('./src/handlers/addPlayerHandler.js');
 
 let game = new Game();
 
