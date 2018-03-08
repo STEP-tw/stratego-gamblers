9f8b2515ddf0efeda8a1ed37cd18c73a94bd4306
ashish mahindrakar <ashishmahi46@gmail.com>
2018-03-08 13:23:14 +0530

    [#69] Ashish/Sayima - added functionality for making retrive query.
    What changes have you made?
    
    * added functionality for retriving all row as well as rows passing
    given conditions.
    
    Why do we need this change?
    
    * to create a query statement for retriving rows based on given condition.
    
    What can this change impact?

--- src/lib/dbManager.js
+++ src/lib/dbManager.js
@@ -3,5 +3,11 @@ dbManager = {
   makeInsertQuery: function (tableName, attributes, values) {
     return `insert into ${tableName} (${attributes.join(",")}) values ('${values.join("','")}');`;
   },
+  makeRetrieveAllQuery(tableName){
+    return `select * from ${tableName};`;
+  },
+  makeRetrieveQueryOf(tableName,condition){
+    return `select * from ${tableName} where ${condition};`;
+  }
 };
 module.exports = dbManager;
--- test/libTest/dbManagerTest.js
+++ test/libTest/dbManagerTest.js
@@ -7,4 +7,16 @@ describe('dbManager', () => {
       assert.equal(dbManager.makeInsertQuery('students',['id'],[1]),expected);
     });
   });
+  describe('#makeRetrieveAllQuery', () => {
+    it('should return query for select all statement', () => {
+      let expected='select * from students;';
+      assert.equal(dbManager.makeRetrieveAllQuery('students'),expected);
+    });
+  });
+  describe('#makeRetrieveQueryOf', () => {
+    it('should give retrieve query for given condition', () => {
+      let expected = 'select * from students where name="sayima";';
+      assert.equal(dbManager.makeRetrieveQueryOf('students','name="sayima"'),expected);
+    });
+  });
 });
